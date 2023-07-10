99c6e34b6f807a7e9b76ed1be730a593659d8b63
Pedro Moraes <pedromoraesv@gmail.com>
2023-07-10 17:48:38 -0300

    add file relative path lualine

--- init.lua
+++ init.lua
@@ -4,14 +4,21 @@ require("ph.telescope")
 require("ph.flutter")
 require("ph.lsp")
 require("ph.lsp")
-require("lualine").setup()
+require("lualine").setup {
+  sections = {
+    lualine_c = { {
+      'filename', 
+      path = 1,
+    } },
+  }
+}
 require("ph.tree")
 require("ph.lazygit")
 require('neorg').setup {
   load = {
     ["core.defaults"] = {}, -- Loads default behaviour
-    ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-    ["core.norg.dirman"] = { -- Manages Neorg workspaces
+    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+    ["core.dirman"] = { -- Manages Neorg workspaces
     config = {
       workspaces = {
         personal = "~/notes/personal",

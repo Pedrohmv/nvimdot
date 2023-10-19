870b329fc9b427d44e9ec474768a49a4cdadaaf3
Pedro Moraes <pedromoraesv@gmail.com>
2023-10-19 14:58:10 -0300

    add new snippets for recoil

--- lua/ph/lsp.lua
+++ lua/ph/lsp.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-    ensure_installed = { "tailwindcss" }
+  ensure_installed = { "tailwindcss" }
 }
 
 -- move to rust.lua
@@ -11,6 +11,12 @@ local nmap = require("ph.keymap").nmap
 local vmap = require("ph.keymap").vmap
 
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.ts,*.tsx,*.js,*.jsx,*.rs,*.css,*.html,*.json,*.scss,*.xml",
+  command = "Prettier",
+  desc = "Format on save"
+})
 
 nmap { "K", vim.lsp.buf.hover }
 nmap { "gd", vim.lsp.buf.definition }
@@ -34,44 +40,44 @@ end)
 local cmp = require("cmp")
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        end,
-    },
-    window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs( -4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    }, {
-        { name = 'buffer' },
-    })
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+  })
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("lspconfig").tsserver.setup {
-    capabilities = capabilities,
+  capabilities = capabilities,
 }
 
 require("lspconfig").cssls.setup {
-    capabilities = capabilities,
+  capabilities = capabilities,
 }
 
 require("lspconfig").lua_ls.setup {
-    capabilities = capabilities,
+  capabilities = capabilities,
 }
 
 require("lspconfig").tailwindcss.setup {
-    capabilities = capabilities,
+  capabilities = capabilities,
 }
